de495ee82cc0b436a8b954416bed92a61ee30b74
ornament1 <aanwulidiunor@gmail.com>
2024-11-18 16:09:06 +0100

    Add files via upload

--- cos101physics assignment.py
+++ cos101physics assignment.py
@@ -29,22 +29,44 @@ def calculate_kinetic_energy(mass, velocity):
     return 0.5 * mass * velocity ** 2
 
 # User input
-mass = float(input("Enterthe mass (in kg): "))
+mass = float(input("Enter the mass (in kg): "))
 velocity = float(input("Enter the velocity (in m/s): "))
 
 # Output
 print(f"The kinetic energy is {calculate_kinetic_energy(mass, velocity)} J (Joule).")
 
 
-# Gravitational Force: F = G * (m1 * m2) / r^2
-def gravitational_force(m1, m2, r):
-    G = 6.67430e-11  # Gravitational constant
-    return G * (m1 * m2) / r**2
+# Program to calculate gravitational potential energy
+def calculate_potential_energy(mass, gravity, height):
+    return mass * gravity * height
 
-# Coulomb’s Law: F = k * |q1 * q2| / r^2
-def electrostatic_force(q1, q2, r):
-    k = 8.9875e9  # Coulomb's constant (N·m²/C²)
-    return k * abs(q1 * q2) / r**2
+# User input
+mass= float(input("Enter the mass (in kg): "))
+height = float(input("Enter the height (in meters): "))
+gravity = 9.81
+# Standard gravitational acceleration (m/s^2)
+
+#Output
+print(f"The gravitational potential energy is {calculate_potential_energy(mass, gravity, height)} J (Joule).")
+
+
+# Program to calculate the force between two charges using Coulomb's Law
+def calculate_coulomb_force(q1, q2, r):
+    k = 8.99e9
+# Coulomb's constant in N.m^2 / C^2
+    if r == 0:
+        return  "Distance between charges cannot be zero!"
+    return k * (q1 * q2) / (r ** 2)
+
+# User input
+q1 = float(input("Enter the magnitude of charge 1 (in Coulombs): "))
+q2 = float(input("Enter the magnitude of charge 2 (in Coulumbs): "))
+r = float(input("Enter the distance between the charges (in meters): "))
+
+#Output
+force = calculate_coulomb_force(q1, q2, r)
+print(f"The force between the charges is {force:.2e} N (Newtons).")
+
 
 
 
